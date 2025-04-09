e64668155760c08b7a8ddb93332e1edf32be4e0f
aashwijat <aashwija.thantry@gmail.com>
2025-04-09 12:56:19 +0530

    Create feedback_entry.py

---
+++ feedback_entry.py
@@ -0,0 +1,23 @@
+## feedback collector
+
+print("--STUDENT FEEDBACK COLLECTOR--")
+
+# student details
+name = str(input("Name : "))
+srn = str(input("SRN : "))
+
+## feedback
+print("Enter Feedback")
+jt = str(input("Java Technologies : "))
+ecom = str(input("Ecommerce Development : "))
+se = str(input("Software Engineering : "))
+vcs = str(input("Version Control Systems : "))
+cie = str(input("Entrepreneurship : "))
+
+##summary
+print("---FEEDBACK SUMMARY---")
+print("Java Technologies : ",jt)
+print("Ecommerce Development : ",ecom)
+print("Software Engineering : ",se)
+print("Version Control System : ",vcs)
+print("Entrepreneurship : ",cie)
